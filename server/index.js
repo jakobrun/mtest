3abf6422d76fd3b87ded11d01f2583a7907fe243
jakobrun <jakob.runarsson@gmail.com>
2014-11-25 22:32:23 +0000

    ref

--- server/index.js
+++ server/index.js
@@ -1,29 +1,34 @@
 'use strict';
 var express = require('express'),
-	app = express();
+    app = express();
 
 app.use(express.static(__dirname + '/../app'));
 
-var data = [
-	{nafn: 'Anne', heimili: 'Bananastreet 5'},
-	{nafn: 'John', heimili: 'Wonderland 27'},
-	{nafn: 'Bill', heimili: 'Sesamestreet 14'},
-	{nafn: 'Sed', heimili: 'Dreamland 7'},
-	{nafn: 'Rose', heimili: 'Dreamland 9'}
-];
+var data = [{
+    nafn: 'Anne',
+    heimili: 'Bananastreet 5'
+}, {
+    nafn: 'John',
+    heimili: 'Wonderland 27'
+}, {
+    nafn: 'Bill',
+    heimili: 'Sesamestreet 14'
+}, {
+    nafn: 'Sed',
+    heimili: 'Dreamland 7'
+}, {
+    nafn: 'Rose',
+    heimili: 'Dreamland 9'
+}];
 
-app.get('/search', function  (req, res) {
-	var q = req.param('q');
-	if(!q) {
-		res.json([]);
-	} else {
-		q = q.toLowerCase();
-		res.json(data.filter(function (p) {
-			return p.nafn.toLowerCase().indexOf(q) !== -1 || p.heimili.toLowerCase().indexOf(q) !== -1;
-		}));		
-	}
+app.get('/search', function(req, res) {
+    var q = req.param('q');
+    q = q && q.toLowerCase();
+    res.json(q ? data.filter(function(p) {
+        return p.nafn.toLowerCase().indexOf(q) !== -1 || p.heimili.toLowerCase().indexOf(q) !== -1;
+    }) : []);
 });
 
-app.listen(3000, function () {
-	console.log('server runing on port 3000');
+app.listen(3000, function() {
+    console.log('server runing on port 3000');
 });
